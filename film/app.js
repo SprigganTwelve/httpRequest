6f2bec4cc98151a62a5c9cdfa4f9d224b1c80bbc
Paterne <angepaterne.dali@gmail.com>
2024-03-07 15:07:58 +0100

    serie

--- film/app.js
+++ film/app.js
@@ -5,6 +5,7 @@ var trunk = document.querySelector("#trunk");
 var filter = document.querySelector("select");
 var bool = true;
 let urlOne = "https://www.omdbapi.com/?apikey=" + key + "&s=movie";
+var post;
 
 fetch(urlOne)
   .then((promise) => {
@@ -16,13 +17,16 @@ startBtn.addEventListener("click", () => {
   if (filter.value == "" && bool) {
     let url = "http://www.omdbapi.com/?apikey=" + key + "&t=" + input.value + "";
     axios.get(url).then((promise) => {
-      let post = `   
+      trunk.textContent = "";
+
+      post = `   
          
               <img src="${promise.data.Poster}" alt="" />
               <span>${promise.data.Title}</span>
               <span>${promise.data.Year}</span>
               <button>Ajouter</button>
         `;
+
       let section = document.createElement("section");
       section.innerHTML = post;
       trunk.appendChild(section);
@@ -32,10 +36,26 @@ startBtn.addEventListener("click", () => {
     let urlS = "http://www.omdbapi.com/?apikey=" + key + "&t=" + input.value + "&s=" + filter.value;
     fetch(urlS)
       .then((promise) => {
-        return promise.json;
+        return promise.json();
       })
       .then((data) => {
-        console.log(data);
+        console.log("input value : ", input.value, " ; url :", urlS, "; data : ", data);
+        trunk.textContent = "";
+        data.Search.forEach((element) => {
+          post = `   
+         
+          <img src="${element.Poster}" alt="" />
+          <span>${element.Title}</span>
+          <span>${element.Year}</span>
+          <button>Ajouter</button>
+    `;
+          let section = document.createElement("section");
+          section.innerHTML = post;
+          trunk.appendChild(section);
+          bool = false;
+        });
       });
   }
 });
+
+/*------------------------------------------*/
