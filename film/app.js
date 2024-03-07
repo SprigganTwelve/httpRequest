a58fa8389aba4af04827f439176c365437ff8b41
Paterne <angepaterne.dali@gmail.com>
2024-03-07 15:20:07 +0100

    change 'go' button application

--- film/app.js
+++ film/app.js
@@ -7,55 +7,37 @@ var bool = true;
 let urlOne = "https://www.omdbapi.com/?apikey=" + key + "&s=movie";
 var post;
 
-fetch(urlOne)
-  .then((promise) => {
-    return promise.json();
-  })
-  .then((data) => {});
+// fetch(urlOne)
+//   .then((promise) => {
+//     return promise.json();
+//   })
+//   .then((data) => {});
 
 startBtn.addEventListener("click", () => {
-  if (filter.value == "" && bool) {
-    let url = "http://www.omdbapi.com/?apikey=" + key + "&t=" + input.value + "";
-    axios.get(url).then((promise) => {
-      trunk.textContent = "";
-
-      post = `   
-         
-              <img src="${promise.data.Poster}" alt="" />
-              <span>${promise.data.Title}</span>
-              <span>${promise.data.Year}</span>
-              <button>Ajouter</button>
-        `;
-
-      let section = document.createElement("section");
-      section.innerHTML = post;
-      trunk.appendChild(section);
-      bool = false;
-    });
-  } else {
-    let urlS = "http://www.omdbapi.com/?apikey=" + key + "&t=" + input.value + "&s=" + filter.value;
-    fetch(urlS)
-      .then((promise) => {
-        return promise.json();
-      })
-      .then((data) => {
+  let urlS = "http://www.omdbapi.com/?apikey=" + key + "&s=" + input.value;
+  fetch(urlS)
+    .then((promise) => {
+      return promise.json();
+    })
+    .then((data) => {
+      if (filter.value == "" && bool) {
         console.log("input value : ", input.value, " ; url :", urlS, "; data : ", data);
         trunk.textContent = "";
         data.Search.forEach((element) => {
           post = `   
-         
-          <img src="${element.Poster}" alt="" />
-          <span>${element.Title}</span>
-          <span>${element.Year}</span>
-          <button>Ajouter</button>
-    `;
+     
+      <img src="${element.Poster}" alt="" />
+      <span>${element.Title}</span>
+      <span>${element.Year}</span>
+      <button>Ajouter</button>
+  `;
           let section = document.createElement("section");
           section.innerHTML = post;
           trunk.appendChild(section);
           bool = false;
         });
-      });
-  }
+      }
+    });
 });
 
 /*------------------------------------------*/
