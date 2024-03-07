9d449996c2a4a79e6ec12b456f4d1ad5edb31ec8
Paterne <angepaterne.dali@gmail.com>
2024-03-07 17:43:29 +0100

    following

--- countryGame/app.js
+++ countryGame/app.js
@@ -1,10 +1,16 @@
 var container = document.querySelector(".container");
 var field = document.querySelector("input");
+var result = document.querySelector(".output");
 const url = "https://countryapi.io/api/all?apikey=j9Mm0tn3jdruyqIHOi7MWd6Xn90CHY6BUeKsR4Vq";
 var ul = document.querySelector("ul");
+var win = 0;
+var lose = 0;
+
+var value = ouptut(url);
+console.log(value);
 field.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    ouptut(url);
+    correction();
   }
 });
 
@@ -18,8 +24,6 @@ function ouptut(url) {
       var randomCountry = getRandomInt(length);
       let objectSelected = Object.keys(data)[randomCountry];
       let value = data[objectSelected];
-      console.log(value);
-      console.log(value.flag);
       ul.children[0].setAttribute("src", `${value.flag.medium}`);
       ul.children[1].textContent = value.name;
       ul.children[2].textContent = "Quel est la Capitale de ce pays ?";
@@ -28,4 +32,31 @@ function ouptut(url) {
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
+}
+
+function correction(answer, value, item, win, lose) {
+  item.textContent = "";
+  if (answer == value.capital) {
+    let succes = document.createElement("div");
+    score += 1;
+    succes.innerHTML = `          <div id="succes" >
+    <span>vous avez  gagner !</span>
+    <button>Follow
+    </button>
+    <p>${win} ${lose}</p>
+  </div>
+>`;
+    item.appendChild(succes);
+  } else {
+    let lose = document.createElement("div");
+    score += 1;
+    succes.innerHTML = `<div id="succes" >
+    <span>vous avez  perdu !</span>
+    <button>Follow
+    </button>
+    <p> Win : ${win}  lose : ${lose}</p>
+  </div>
+>`;
+    item.appendChild(lose);
+  }
 }
