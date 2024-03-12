608a6f47bb5b7bae06df9bebf33ffc2ea88b0a17
Paterne <angepaterne.dali@gmail.com>
2024-03-12 10:40:06 +0100

    features(skip, sore)

--- countryGame/app.js
+++ countryGame/app.js
@@ -1,19 +1,38 @@
 var container = document.querySelector(".container");
 var field = document.querySelector("input");
-var result = document.querySelector(".output");
+var result = document.querySelector(".result");
 const url = "https://countryapi.io/api/all?apikey=j9Mm0tn3jdruyqIHOi7MWd6Xn90CHY6BUeKsR4Vq";
 var ul = document.querySelector("ul");
+var skip = document.querySelector("#skip");
+var go = document.querySelector("#go");
+var score = document.querySelector("#score");
 var win;
 var lose;
 var inf;
 
-var value = ouptut(url);
+ouptut(url);
+
+skip.addEventListener("click", () => {
+  ouptut(url);
+});
+
+go.addEventListener("click", () => {
+  result.innerHTML = "";
+  result.textContent = correction(inf, field.value, win, lose);
+});
 
 field.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
+    result.innerHTML = "";
     result.textContent = correction(inf, field.value, win, lose);
   }
 });
+
+score.addEventListener("click", () => {
+  alert(`Votre Score actuel est : \n Win :${win} ; Lose : ${lose} `);
+});
+
+/*------------function-----------*/
 
 function ouptut(url) {
   fetch(url)
@@ -26,13 +45,12 @@ function ouptut(url) {
       let objectSelected = Object.keys(data)[randomCountry];
       let value = data[objectSelected];
       inf = value;
+      console.log(inf);
       ul.children[0].setAttribute("src", `${value.flag.medium}`);
       ul.children[1].textContent = value.name;
       ul.children[2].textContent = "Quel est la Capitale de ce pays ?";
     });
 }
-
-/*------------function-----------*/
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -66,7 +84,7 @@ function correction(answer, value, item, win, lose) {
 }
 
 function correction(inf, val, win, lose) {
-  if (inf.capital == val) {
+  if (inf.capital.trim().toLowerCase() === val.trim().toLowerCase()) {
     win++;
     return "You Win";
   } else {
